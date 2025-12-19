6dc64920127dcf1603a7e0736e1723a5bc1c4685
anshjw <anshjw1926@gmail.com>
2025-12-19 08:27:52 +0530

    chat bot readded

--- app/layout.js
+++ app/layout.js
@@ -16,6 +16,7 @@ export default function RootLayout({ children }) {
       <body className="min-h-screen flex flex-col bg-cream text-slate">
         <SessionProvider>
           <Navbar />
+          <script src='https://www.noupe.com/embed/019b3379d0fb717cb2ab08c3dc55a4315c07.js'></script>
           <main className="flex-1">{children}</main>
           <Footer />
         </SessionProvider>
